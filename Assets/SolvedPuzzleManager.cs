608e7d8e2ae3554d6d915d27c0694f6357da38d1
Safa Mohammed <sumohammed0@gmail.com>
2025-04-12 17:09:09 -0500

    glowing orb

---
+++ Assets/SolvedPuzzleManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class SolvedPuzzleManager : MonoBehaviour
+{
+    public bool puzzle1Solved = false;
+    public bool puzzle2Solved = false;
+    public bool puzzle4Solved = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void solvedPuzzle1() {
+        Debug.Log("Puzzle 1 Solved");
+        puzzle1Solved = true;
+    }
+
+    public void solvedPuzzle2() {
+        Debug.Log("Puzzle 2 Solved");
+        puzzle2Solved = true;
+    }
+
+    public void solvedPuzzle4() {
+        Debug.Log("Puzzle 4 Solved");
+        puzzle4Solved = true;
+    }
+}
