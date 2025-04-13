5ec7875435a57bf383d6a47318805bbb31e98bf8
Safa Mohammed <sumohammed0@gmail.com>
2025-04-13 12:48:39 -0500

    orb interaction

--- Assets/OrbInteraction.cs
+++ Assets/OrbInteraction.cs
@@ -5,36 +5,29 @@ public class OrbInteraction : MonoBehaviour
     public GameObject orb;
     public bool isOrbAboveChild = false;
     public SolvedPuzzleManager solvedPuzzleManagerScript;
+    public float acceptableRange = 0.3f;
+    public Transform targetTransform;
+    public RaycastHandler raycastHandlerScript;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         solvedPuzzleManagerScript = GameObject.FindGameObjectWithTag("SolveManager").GetComponent<SolvedPuzzleManager>();
+        targetTransform = this.transform.GetChild(0).GetChild(0);
+        Debug.Log("white image position: " + targetTransform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Get the child of the child of the current GameObject
-        Transform childOfChild = transform.GetChild(0).GetChild(0);
+        float distance = Vector3.Distance(orb.transform.position, targetTransform.position);
 
-        // Check if the orb is directly above the child of the child
-        if (Mathf.Approximately(orb.transform.position.x, childOfChild.position.x) &&
-            Mathf.Approximately(orb.transform.position.z, childOfChild.position.z) &&
-            orb.transform.position.y > childOfChild.position.y)
+        if (distance <= acceptableRange && !raycastHandlerScript.isGrabbing)
         {
+            Debug.Log("Sphere is placed within the acceptable range.");
+            // You can trigger logic here, e.g., set a flag or play an animation
             isOrbAboveChild = true;
-            Debug.Log("Orb is above the child of the child.");
             solvedPuzzleManagerScript.solvedPuzzle1();
-        }
-        else
-        {
-            isOrbAboveChild = false;
-        }
-
-        if (orb.transform.position.y < 0)
-        {
-            orb.transform.position = new Vector3(orb.transform.position.x, 0, orb.transform.position.z);
         }
 
     }
